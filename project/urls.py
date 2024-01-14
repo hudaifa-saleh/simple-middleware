afc4e37ea1977c2c0f1eabe18e7b176d6f889a84
hudaifa <hudaifa890@gmail.com>
2024-01-14 23:07:21 +0300

    a basic payment app

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from bank import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.process_payment, name='payment'),
 ]
